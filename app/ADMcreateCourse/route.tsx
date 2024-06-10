116bd6997232c7668081b75587c776fc8c3bbcbb
Mateusz Zalewski <mateuszzalewski18@gmail.com>
2024-06-10 14:52:26 +0000

    utf8 encoding

--- app/ADMcreateCourse/route.tsx
+++ app/ADMcreateCourse/route.tsx
@@ -3,6 +3,7 @@ import validateSession from "@/functions/validateSession"
 import ADMcourseElement from "@/interfaces/ADMcourseElement"
 import { badRequest, notFound, unauthorized } from "@/responses/responses"
 import { NextResponse } from "next/server"
+import utf8 from "utf8"
 
 export async function POST(req: Request, res: Response){
     const headers = req.headers
@@ -19,7 +20,7 @@ export async function POST(req: Request, res: Response){
     const db = await getDatabase()
     const validatedUser = await validateSession(db, sessionID)
     if (!validatedUser) { return unauthorized }
-    const insteredCourseArray = await db.query('INSERT INTO "courses"("date", "title", "place", "instructor", "note", "price", "span", "slots", "available") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, true) RETURNING *', [date, title, place, instructor, note, price, span, slots])
+    const insteredCourseArray = await db.query('INSERT INTO "courses"("date", "title", "place", "instructor", "note", "price", "span", "slots", "available") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, true) RETURNING *', [date, utf8.decode(title), utf8.decode(place), utf8.decode(instructor), utf8.decode(note), price, span, slots])
     if (!insteredCourseArray || insteredCourseArray.rowCount == 0) { return badRequest }
     var insertedCourse: ADMcourseElement = (await Promise.all(insteredCourseArray.rows.map(async (result) => ({ id: result.id, date: result.date, span: result.span, price: result.price, title: result.title, place: result.place, instructor: result.instructor, note: result.note, slots: result.slots, slotsUsed: 0, available: result.available }) )))[0]
     return NextResponse.json(insertedCourse, {status: 200})
--- app/ADMeditCourse/route.tsx
+++ app/ADMeditCourse/route.tsx
@@ -4,6 +4,7 @@ import validateSession from "@/functions/validateSession"
 import ADMcourseElement from "@/interfaces/ADMcourseElement"
 import { badRequest, notFound, unauthorized } from "@/responses/responses"
 import { NextResponse } from "next/server"
+import utf8 from "utf8"
 
 export async function PATCH(req: Request, res: Response){
     const headers = req.headers
@@ -23,7 +24,7 @@ export async function PATCH(req: Request, res: Response){
     if (!validatedUser) { return unauthorized }
     const courseFoundArray = await db.query('SELECT * FROM "courses" WHERE "id" = $1 LIMIT 1', [courseID])
     if (!courseFoundArray || courseFoundArray.rowCount == 0) { return notFound }
-    const changedCoursesArray = await db.query('UPDATE "courses" SET "date" = $1, "title" = $2, "place" = $3, "instructor" = $4, "note" = $5, "price" = $6, "span" = $7, "slots" = $8 WHERE "id" = $9 RETURNING *', [date, title, place, instructor, note, price, span, slots, courseID])
+    const changedCoursesArray = await db.query('UPDATE "courses" SET "date" = $1, "title" = $2, "place" = $3, "instructor" = $4, "note" = $5, "price" = $6, "span" = $7, "slots" = $8 WHERE "id" = $9 RETURNING *', [date, utf8.decode(title), utf8.decode(place), utf8.decode(instructor), utf8.decode(note), price, span, slots, courseID])
     if (!changedCoursesArray || changedCoursesArray.rowCount == 0) { return badRequest }
     var changedCourse: ADMcourseElement = (await Promise.all(changedCoursesArray.rows.map(async (result) => ({ id: result.id, date: result.date, span: result.span, price: result.price, title: result.title, place: result.place, instructor: result.instructor, note: result.note, slots: result.slots, slotsUsed: await getCourseSignupCount(db, result.id), available: result.available }) )))[0]
     return NextResponse.json(changedCourse, {status: 200})
