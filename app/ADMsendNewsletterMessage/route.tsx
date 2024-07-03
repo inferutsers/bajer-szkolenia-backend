bfd120eef6e5b58b662d3776a182fbfaa7679a2b
Mateusz Zalewski <mateuszzalewski18@gmail.com>
2024-07-03 12:49:08 +0000

    encoding

--- app/ADMsendNewsletterMessage/route.tsx
+++ app/ADMsendNewsletterMessage/route.tsx
@@ -4,6 +4,7 @@ import sendNewsletterEmail from "@/functions/sendNewsletterEmail"
 import validateSession from "@/functions/validateSession"
 import { badRequest, notFound, unauthorized, unprocessableContent } from "@/responses/responses"
 import { NextResponse } from "next/server"
+import utf8 from 'utf8'
 
 export async function POST(req: Request, res: Response){
     const headers = req.headers,
@@ -16,7 +17,7 @@ export async function POST(req: Request, res: Response){
     if (!validatedUser) { return unauthorized }
     const newsletterEmails = await gatherNewsletterEmails(db)
     if (!newsletterEmails) { return notFound }
-    const mailSent = await sendNewsletterEmail(db, subject, message, newsletterEmails)
+    const mailSent = await sendNewsletterEmail(db, utf8.decode(subject), utf8.decode(message), newsletterEmails)
     if (!mailSent) { return unprocessableContent }
     return NextResponse.json(newsletterEmails.length, {status: 200})
 }
