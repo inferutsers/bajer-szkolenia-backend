621e0d220824e835a5906ebd9b03e3319aa80387
Mateusz Zalewski <mateuszzalewski18@gmail.com>
2024-08-12 15:37:55 +0000

    db return fixes

--- functions/queries/offer.tsx
+++ functions/queries/offer.tsx
@@ -2,6 +2,7 @@
 import { Pool } from "pg";
 import offerElement from "@/interfaces/offerElement";
 import getOfferCourses from "../getOfferCourses";
+import { getDateLong } from "../dates";
 
 export async function uploadFile(db: Pool, id: string | number, file: Buffer, fileName: string): Promise<boolean>{
     const course = await db.query('UPDATE "offers" SET "file" = $1, "fileName" = $2 WHERE "id" = $3', [file, fileName, id])
@@ -16,7 +17,7 @@ export async function deleteFile(db: Pool, id: string | number): Promise<boolean
 }
 
 export async function createOffer(db: Pool, name: string, note: string | undefined = undefined, price: string, courses: number[]): Promise<offerElement | undefined> {
-    const offer = await db.query('INSERT INTO "offers"("name", "note", "price", "courses") VALUES ($1, $2, $3, $4)', [name, note, price, courses])
+    const offer = await db.query('INSERT INTO "offers"("name", "note", "price", "courses", "dateCreated") VALUES ($1, $2, $3, $4, $5) RETURNING *', [name, note, price, courses, getDateLong()])
     if (!offer || offer.rowCount == 0) { return undefined }
     return await formatAsOfferElement(offer.rows[0], db, true)
 }
@@ -28,7 +29,7 @@ export async function deleteOffer(db: Pool, id: string): Promise<boolean>{
 }
 
 export async function updateOffer(db: Pool, id: string, name: string, note: string | undefined = undefined, price: string): Promise<offerElement | undefined> {
-    const offer = await db.query('UPDATE "offers" SET "name" = $1, "note" = $2, "price" = $3 WHERE "id" = $4', [name, note, price, id])
+    const offer = await db.query('UPDATE "offers" SET "name" = $1, "note" = $2, "price" = $3 WHERE "id" = $4 RETURNING *', [name, note, price, id])
     if (!offer || offer.rowCount == 0) { return undefined }
     return await formatAsOfferElement(offer.rows[0], db, true)
 }
