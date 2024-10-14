aeab4f4d4e53c5c069ff59a357a5011a1de88540
Mateusz Zalewski <mateuszzalewski18@gmail.com>
2024-10-14 13:48:57 +0000

    cleanup

--- app/AUTOCRON/coursereminders/route.tsx
+++ app/AUTOCRON/coursereminders/route.tsx
@@ -1,11 +1,8 @@
 import getDatabase from "@/connection/database"
 import validateCRONSecret from "@/functions/AUTOCRON/validateCRONSecret"
 import { getDateLongGMT2Readable } from "@/functions/dates"
-import sendSingleEmail from "@/functions/emails/processor/sendSingleEmail"
-import { getCourseSignups } from "@/functions/getCourseSignups"
 import { systemAction, systemActionStatus } from "@/functions/logging/actions"
 import { systemLog } from "@/functions/logging/log"
-import { ADMlockDueCourses, getUpcomingCourses } from "@/functions/queries/course"
 import { sendCourseReminders } from "@/functions/sendCourseReminders"
 import { rm001000, rm001001 } from "@/responses/messages"
 import { badRequest, unauthorized } from "@/responses/responses"
@@ -19,7 +16,7 @@ export async function GET(req: Request, res: Response){
     const db = await getDatabase(req)
     const signupsReminded = await sendCourseReminders(db)
     if (signupsReminded > 0) {
-        systemLog(systemAction.AUTOCRONlockcourses, systemActionStatus.success, `Powiadomiono ${signupsReminded} zapisów`, undefined, db)
+        systemLog(systemAction.AUTOCRONcoursereminders, systemActionStatus.success, `Powiadomiono ${signupsReminded} zapisów`, undefined, db)
     }
     return NextResponse.json(`${getDateLongGMT2Readable()} >>> Notified ${signupsReminded} signups...`, {status: 200})
 }
